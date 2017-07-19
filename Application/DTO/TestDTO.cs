2d3243f496c4dcf62048ad4a43fdbd6d1ce93d92
Luoyz <290330505@qq.com>
2017-07-19 17:17:38 +0800

    #3

---
+++ Application/DTO/TestDTO.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO
+{
+    public class TestDTO : TestObjs
+    {
+        public TestDTO(TestObjs obj)
+        {
+            this.Id = obj.Id;
+            this.ForeinKey = obj.ForeinKey;
+            this.Name = obj.Name;
+            this.TestForeign = obj.TestForeign;
+        }
+        public string Fname { get; set; }
+    }
+}
